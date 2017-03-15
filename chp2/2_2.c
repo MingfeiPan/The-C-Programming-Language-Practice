4ff2e87e1fd5ad404742e0b5c7b52ccc99b950e4
Mingfei <113104667@umail.ucc.ie>
2017-03-15 21:53:45 +0800

    limits

---
+++ chp2/2_2.c
@@ -0,0 +1,15 @@
+/*
+ *for loop without && ||
+*/
+
+for (i=0; i<lim-1 && (c=getchar()) != '\n' && c != EOF; ++i) 
+    s[i] = c;
+
+
+int i = 0;
+while(i < lim -1){
+    c = getchar();
+    if(c == '\n') break;
+    if(c == EOF) break;
+    s[i++] = c;
+}
